c88e78668b44ce33a9233d3a773d77876141c5f6
firthunsps@gmail.com <firthunsps@gmail.com>
2021-03-23 18:15:59 +0100

    initial commit

---
+++ src/app/gel/pipes/imagen.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Equiposgel} from '../interfaces/equipogel.interface';
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform( equipo: Equiposgel): string {
+
+    console.log('Pipe imagen procesado', equipo);
+
+    // CONTROLANDO  LAS EXCEPTIONES
+
+    if (!equipo.ticketcompra){
+      return `assets/drop-images.png`;
+    }
+    return `assets/${ equipo.ticketcompra }.jpg`;
+  }
+
+
+}
