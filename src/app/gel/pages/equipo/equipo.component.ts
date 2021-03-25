89cd7244a1ce8b8b6992d5dd734e0077730e84b7
firthunsps@gmail.com <firthunsps@gmail.com>
2021-03-25 10:44:21 +0100

    initial commit

--- src/app/gel/pages/equipo/equipo.component.ts
+++ src/app/gel/pages/equipo/equipo.component.ts
@@ -1,16 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,  OnInit} from '@angular/core';
+import {Equiposgel} from '../../interfaces/equipogel.interface';
+import {ActivatedRoute, Router} from '@angular/router';
+import {GelService} from '../../services/gel.service';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-equipo',
   templateUrl: './equipo.component.html',
   styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 15px;
+      }
+    `
   ]
 })
-export class EquipoComponent implements OnInit {
+export class EquipoComponent  implements OnInit{
 
-  constructor() { }
+
+  // con el el signo '!', le indico a TS que confie en que llegará info
+  equipo!: Equiposgel;
+  id: string = '';
+
+  /*leemos nuestra Url, utilizando ActivateRoute */
+      constructor(private activateRoute: ActivatedRoute,
+                  private router       : Router,
+                  private gelServices  : GelService) {  }
 
   ngOnInit(): void {
+
+        // this.activateRoute.params
+        //   .subscribe(  ({ id }) => console.log( id ) );
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id })  => this.gelServices.getEquipoPorId(id))
+      )
+      .subscribe( resp => this.equipo = resp);
+
   }
 
+
+  regresar() {
+    this.router.navigate(['/equipos/listado']);
+  }
 }
